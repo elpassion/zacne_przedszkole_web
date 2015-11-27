09871279e7fc6528d74377db1a9a02f33474b619
Patryk Wojtyński <patryk.wojtynski@elpassion.pl>
2015-11-27 13:14:15 +0100

    Missing kindergarten page

--- src/components/Kindergarten.jsx
+++ src/components/Kindergarten.jsx
@@ -117,7 +117,7 @@ export default class Kindergarten extends React.Component {
   }
 
   kindergatenNotLoaded() {
-    console.log('Reirect here!!!!!');
+    this.props.history.pushState(null, '/przedszkole/brak', {});
   }
 
 }
